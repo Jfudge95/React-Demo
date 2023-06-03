dc46490dacdc72bb576aeec2afa0abdf451105f3
Jordan Fudge <jordanfudge95@gmail.com>
2023-06-03 18:56:37 -0400

    Added useEffect in counter

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,9 +1,13 @@
-import { useState } from "react"; //state = a value that when it changes the DOM reflects that change. Stateful variables must have the useState hook here.
+import { useState, useEffect } from "react"; //state = a value that when it changes the DOM reflects that change.
 
 function Counter() {
   const [counter, setCounter] = useState(0);
 
-  const increment = () => {
+  useEffect(() => {
+    console.log("counter:", counter);
+  }, [counter]);
+
+  const increment = (e) => {
     setCounter((currentVal) => currentVal + 1); //Because multiple things could be changing our Counter. We use currentVal to always show the current value of the counter
   };
 
@@ -23,3 +27,4 @@ function Counter() {
 export default Counter;
 
 // The first value "counter" is our stateful variable. Our second "setCounter" is our variable. Inside the function we give it the initial value of zero useState(0)
+// In our useEffect, We will see in our console.log that everyime ourr array with the stateful variable changes (aka [counter]) we will see counter: # each time the stateful cariable changes.
