3ae3450293ed7b3261e092530ba4e922900539bb
Jordan Fudge <jordanfudge95@gmail.com>
2023-06-03 19:37:07 -0400

    Added Additional Notes

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -27,4 +27,6 @@ function Counter() {
 export default Counter;
 
 // The first value "counter" is our stateful variable. Our second "setCounter" is our variable. Inside the function we give it the initial value of zero useState(0)
-// In our useEffect, We will see in our console.log that everyime ourr array with the stateful variable changes (aka [counter]) we will see counter: # each time the stateful cariable changes.
+// Dependancy Array - This is how we control when it runs. Since we want my profile page to display right when we load, we leave that array empty. However, if we put a STATEFUL VARIABLE into the array, it will only run when that Stateful Variable changes.
+// In our useEffect, We will see in our console.log that everyime Dependancy Array with the stateful variable changes (aka [counter]) we will see counter: # each time the stateful variable changes. IF There was more than 1 staeful variable you would just add a comma "," and add however many stateful variables you want to keep track of for that useEffect
+// By default, useEffect will run on each render.
